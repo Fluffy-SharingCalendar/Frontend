de35d637d54fd7799285c6c60d0230c1bc180bc8
sunga <tjddk6662@naver.com>
2024-11-29 23:06:01 +0900

    Initial Commit

---
+++ Fluffy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fluffy
+//
+//  Created by 손성아 on 11/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fluffy/FluffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluffyApp.swift
+//  Fluffy
+//
+//  Created by 손성아 on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FluffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
