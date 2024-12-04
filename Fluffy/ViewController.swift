0974b8e328aaf42f17f9b182ea5ce71ac7fb5329
sunga <tjddk6662@naver.com>
2024-12-04 21:55:06 +0900

    re-initial commit

---
+++ Fluffy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fluffy
+//
+//  Created by 손성아 on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FluffyTests/FluffyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluffyTests.swift
+//  FluffyTests
+//
+//  Created by 손성아 on 12/4/24.
+//
+
+import Testing
+@testable import Fluffy
+
+struct FluffyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
