6ebe4249343ed102e5bcf20ac9543c2311f3bb3a
sunga <tjddk6662@naver.com>
2024-12-17 00:15:57 +0900

    feat : Navigation Controller 추가
    로그인 화면에서 "회원가입" 버튼 클릭 시 회원가입으로 화면전환
    Navigation Controller 커스텀 완료
    
    issues : #4

---
+++ Fluffy/Common/CustomViews/CustomNavigationBar.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationBar.swift
+//  Fluffy
+//
+//  Created by 손성아 on 12/16/24.
+//
+
+import UIKit
+
+class CustomNavigationBar: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Fluffy/Features/Start/Views/SignUpViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SignUpViewController.swift
+//  Fluffy
+//
+//  Created by 손성아 on 12/16/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+}
