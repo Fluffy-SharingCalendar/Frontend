de9519edff04e1ff33bc99db50954a35434d85a3
sunga <tjddk6662@naver.com>
2025-01-06 01:42:00 +0900

    feat : 커스텀 Q&A 본인확인 필드 구현 완료
    본인 확인 UI/UX 구현완료
    키보드 옵저버 추가
    
    issues : #4

--- Fluffy/Common/CustomViews/CustomQ&A.swift
+++ Fluffy/Common/CustomViews/CustomQ&A.swift
@@ -12,10 +12,15 @@ class CustomQ_A: UIStackView {
     private func answerTextField(_ placeholder: String) -> UITextField {
         let textField = UITextField()
         
-        textField.placeholder = placeholder
+        
         textField.font = UIFont.systemFont(ofSize: 15)
         textField.textColor = UIColor(named: "TextColor")
         textField.tintColor = UIColor(named: "MainColor")
+        
+        textField.attributedPlaceholder = NSAttributedString(
+                string: placeholder,
+                attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "TextColor")!]
+            )
         
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 25, height: textField.frame.height))
         textField.leftView = paddingView
@@ -32,10 +37,14 @@ class CustomQ_A: UIStackView {
     private func questionTextField(_ image: UIImage?, _ placeholder: String) ->  UITextField {
         let textField = UITextField()
         
-        textField.placeholder = placeholder
         textField.font = UIFont.systemFont(ofSize: 15)
         textField.textColor = UIColor(named: "TextColor")
         textField.tintColor = UIColor(named: "MainColor")
+        
+        textField.attributedPlaceholder = NSAttributedString(
+                string: placeholder,
+                attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "TextColor")!]
+            )
         
         // 고정된 높이 설정
         textField.translatesAutoresizingMaskIntoConstraints = false
@@ -61,17 +70,45 @@ class CustomQ_A: UIStackView {
         pullDownButton.contentHorizontalAlignment = .left
         pullDownButton.sizeToFit()
         
-        pullDownButton.setTitle(title, for: .normal)
-        pullDownButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
-        pullDownButton.setTitleColor(.white, for: .normal)
+        
+        var configuration = UIButton.Configuration.plain()
+        
+        configuration.image = image
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = 15
+        
+        var attributedTitle = AttributedString(title)
+        attributedTitle.font = .systemFont(ofSize: 15, weight: .regular)
+        attributedTitle.foregroundColor = UIColor(named: "TextColor")
+        configuration.attributedTitle = attributedTitle
+
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 10)
+        pullDownButton.configuration = configuration
         
         // 고정된 높이 설정
         pullDownButton.translatesAutoresizingMaskIntoConstraints = false
         pullDownButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
+        let options: [String] = ["키우는 반려동물의 이름은?", "아버지의 성함은?", "어머니의 성함은?", "졸업한 초등학교의 이름은?", "졸업한 중학교의 이름은?", "졸업한 고등학교의 이름은?", "나의 생일은?"]
+        
+        let actions = options.map { option in
+                UIAction(title: option) { _ in
+                    // 선택된 옵션을 버튼 제목으로 업데이트
+                    var updatedTitle = AttributedString(option)
+                    updatedTitle.font = .systemFont(ofSize: 15, weight: .regular)
+                    updatedTitle.foregroundColor = UIColor(named: "TextColor")
+                    
+                    configuration.attributedTitle = updatedTitle
+                    pullDownButton.configuration = configuration
+                }
+            }
+        
+        
+        pullDownButton.menu = UIMenu(title: "", children: actions)
+        pullDownButton.showsMenuAsPrimaryAction = true
+    
         return pullDownButton
     }
-    
     
     override init(frame: CGRect) {
             super.init(frame: frame)
@@ -108,6 +145,20 @@ class CustomQ_A: UIStackView {
             self.addArrangedSubview(lineView)
             
             // answerTextField 생성 및 추가
+            let answerField = answerTextField(answerPlaceholder)
+            self.addArrangedSubview(answerField)
+        } else {
+            let questionField = questionPullDownButton(image, questionPlaceholder)
+            self.addArrangedSubview(questionField)
+            
+            let lineView = UIView()
+            lineView.layer.borderColor = UIColor(named: "MainColor")?.cgColor
+            lineView.layer.borderWidth = 1
+            NSLayoutConstraint.activate([
+                lineView.heightAnchor.constraint(equalToConstant: 1)
+                ])
+            self.addArrangedSubview(lineView)
+            
             let answerField = answerTextField(answerPlaceholder)
             self.addArrangedSubview(answerField)
         }
--- Fluffy/Features/Start/Views/SignUpViewController.swift
+++ Fluffy/Features/Start/Views/SignUpViewController.swift
@@ -9,14 +9,19 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordStackView: CustomQ_A!
-    
+    @IBOutlet weak var questionStackView: CustomQ_A!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setupKeybaordObserver()
+        
         passwordStackView.setStackView(true, UIImage(named: "Unlock")!, "비밀번호를 다시 입력하세요.", "비밀번호를 입력하세요.")
         passwordStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        questionStackView.setStackView(false, UIImage(named: "Chat")!, "답변을 입력하세요.", "키우는 반려동물의 이름은?")
+        questionStackView.translatesAutoresizingMaskIntoConstraints = false
     }
 
 }
